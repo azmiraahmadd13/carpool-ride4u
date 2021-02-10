ef4fb1a15562548bcea1ef213a13ed1cd969316f
azmiraahmadd13 <78847584+azmiraahmadd13@users.noreply.github.com>
2021-02-10 22:24:24 +0800

    Update search.php

--- search.php
+++ search.php
@@ -26,7 +26,7 @@
 	<script src="js/sweetalert.min.js"></script> 
  <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
     <script
-      src="https://maps.googleapis.com/maps/api/js?key=&callback=initMap&libraries=&v=weekly"
+      src="https://maps.googleapis.com/maps/api/js?key=YOUR API&callback=initMap&libraries=&v=weekly"
       defer
     ></script>
     <!-- Theme Style -->
